f2985733c5921ee1a710a0e4cab5000bc8cbad01
mil10r <mil10r747@gmail.com>
2023-08-26 13:21:17 +0700

    refactor ghcr_push.sh and publish_docker_multi.sh (2)

--- util/publish_docker_multi.sh
+++ util/publish_docker_multi.sh
@@ -3,27 +3,26 @@
 source .env
 echo $GHCR_TOKEN | docker login ghcr.io -u mil10r --password-stdin
 
-IMAGE_NAME=ghcr.io/mil10r/pr_freqtrade
-TAG=pr_freqtrade
-TAG_PLOT=${TAG}_plot
-TAG_FREQAI=${TAG}_freqai
-TAG_FREQAI_RL=${TAG_FREQAI}rl
+IMAGE_NAME=pr_freqtrade
+GHCR_IMAGE_NAME=ghcr.io/mil10r/${IMAGE_NAME}
+TAG_PLOT=plot
+TAG_FREQAI=freqai
+TAG_FREQAI_RL=rl
 
-docker tag freqtrade:$TAG ${IMAGE_NAME}:$TAG
+docker tag freqtrade:$TAG ${GHCR_IMAGE_NAME}:$TAG
 
-docker build --build-arg sourceimage=freqtrade --build-arg sourcetag=${TAG} -t freqtrade:${TAG_PLOT} -f docker/Dockerfile.plot .
-docker build --build-arg sourceimage=freqtrade --build-arg sourcetag=${TAG} -t freqtrade:${TAG_FREQAI} -f docker/Dockerfile.freqai .
-docker build --build-arg sourceimage=freqtrade --build-arg sourcetag=${TAG_FREQAI} -t freqtrade:${TAG_FREQAI_RL} -f docker/Dockerfile.freqai_rl .
+docker build --build-arg sourceimage=freqtrade --build-arg sourcetag=${TAG} -t ${IMAGE_NAME}:${TAG_PLOT} -f docker/Dockerfile.plot .
+docker build --build-arg sourceimage=freqtrade --build-arg sourcetag=${TAG} -t ${IMAGE_NAME}:${TAG_FREQAI} -f docker/Dockerfile.freqai .
+docker build --build-arg sourceimage=freqtrade --build-arg sourcetag=${TAG_FREQAI} -t ${IMAGE_NAME}:${TAG_FREQAI_RL} -f docker/Dockerfile.freqai_rl .
 
-docker tag freqtrade:$TAG_PLOT ${IMAGE_NAME}:$TAG_PLOT
-docker tag freqtrade:$TAG_FREQAI ${IMAGE_NAME}:$TAG_FREQAI
-docker tag freqtrade:$TAG_FREQAI_RL ${IMAGE_NAME}:$TAG_FREQAI_RL
+docker tag ${IMAGE_NAME}:$TAG_PLOT ${GHCR_IMAGE_NAME}:${TAG_PLOT}
+docker tag ${IMAGE_NAME}:$TAG_FREQAI ${GHCR_IMAGE_NAME}:${TAG_FREQAI}
+docker tag ${IMAGE_NAME}:$TAG_FREQAI_RL ${GHCR_IMAGE_NAME}:${TAG_FREQAI_RL}
 
 docker images
 
-docker push ${IMAGE_NAME}:$TAG
-docker push ${IMAGE_NAME}:$TAG_PLOT
-docker push ${IMAGE_NAME}:$TAG_FREQAI
-docker push ${IMAGE_NAME}:$TAG_FREQAI_RL
+docker push ${GHCR_IMAGE_NAME}:$TAG_PLOT
+docker push ${GHCR_IMAGE_NAME}:$TAG_FREQAI
+docker push ${GHCR_IMAGE_NAME}:$TAG_FREQAI_RL
 
 docker images
